7c5ec9928262dd8831ba68a98485394da9f7f62f
Matheus7k <69181983+Matheus7k@users.noreply.github.com>
2022-10-13 16:06:52 -0300

    initializing project

---
+++ backend/src/server.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import { userRouter } from "./routes/user.routes";
+
+const app = express();
+
+app.use(express.json());
+
+app.use(userRouter);
+
+app.listen(3000, () => {
+  console.log("listening on port 3000!");
+});
