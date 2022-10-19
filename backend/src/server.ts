b0478b99e69908ba72927db0a12433d2171cee15
Matheus7k <69181983+Matheus7k@users.noreply.github.com>
2022-10-19 14:37:02 -0300

    add swagger in application

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,15 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 import { movieRouter } from "./routes/movie.routes";
 import { userRouter } from "./routes/user.routes";
+
+import swaggerConfig from "./swagger.json";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerConfig));
 
 app.use(userRouter);
 app.use(movieRouter);
